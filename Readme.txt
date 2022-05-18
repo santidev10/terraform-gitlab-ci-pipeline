ace0c9b4c6f821243912b2a3ac1507570c0db3dd
Santiago Gallo <santiago@jiko.io>
2022-05-17 19:50:29 -0500

    first commit

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+https://medium.com/@nbqadri/beginners-guide-to-using-terraform-via-gitlab-ci-cd-pipelines-for-aws-a600ca4588c4
+
+Beginner’s guide to using terraform via Gitlab CI/CD pipelines for AWS.
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+default = "My Awesome Infra"
+}
+variable "AWS_DEFAULT_REGION" {
+default = "us-east-1"
+}
+variable "AWS_ACCESS_KEY_ID" {
+default = "AKIARSSOQP2HXY2TRKWG"
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+default = "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"
+}
+variable "AWS_AVAILABILITY_ZONE" {
+default = "us-east-1a"
+}
