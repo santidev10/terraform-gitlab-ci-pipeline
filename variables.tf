393c7b430747fc49727901276b8573cb90686a07
santidev10 <36556977+santidev10@users.noreply.github.com>
2022-05-18 07:51:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,10 +5,10 @@ variable "AWS_DEFAULT_REGION" {
 default = "us-east-1"
 }
 variable "AWS_ACCESS_KEY_ID" {
-default = "AKIARSSOQP2HXY2TRKWG"
+default = ""
 }
 variable "AWS_SECRET_ACCESS_KEY" {
-default = "iIuGEAq7043sO74dlQI3MDzyHRIYZHgYGl78cM5pX"
+default = ""
 }
 variable "AWS_AVAILABILITY_ZONE" {
 default = "us-east-1a"
